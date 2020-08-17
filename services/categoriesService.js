b2c833d082b1a9d5c4679dc923532b03a72e8ecd
Karan Warrier <kawarrier@yahoo.com>
2020-08-17 11:33:06 -0700

    cleaning up more code

---
+++ services/categoriesService.js
@@ -0,0 +1,101 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const assert = require("assert");
+const { MongoClient } = require("mongodb");
+const uri = `mongodb+srv://bgaskwarrier:kaw009020@cluster0-1jamj.mongodb.net/StudentSystem?retryWrites=true&w=majority`;
+const validate = require("jsonschema").validate;
+
+// Schema to compare new POST entries to
+const catSchema = {
+  name: "string",
+  childCategories: {
+    type: "array",
+    items: { type: "object" },
+  },
+  required: ["name", "childCategories"],
+};
+
+exports.postCategory = async function (req, res) {
+  const entry = req.body;
+
+  try {
+    validate(entry, catSchema, { throwError: true });
+  } catch (error) {
+    res.status(401).end("Invalid body format: " + error.message);
+    return;
+  }
+
+  console.log(entry);
+  createEntry(res, req.app.client, entry).catch((error) => {
+    res
+      .status(400)
+      .end("Error in the create category request: " + error.message);
+  });
+};
+
+exports.getCategory = async function (req, res) {
+  const query = req.query;
+  const name = query.name;
+
+  getEntry(res, req.app.client, name).catch((error) => {
+    res.status(400).end("Error in the get category request: " + error.message);
+  });
+};
+
+exports.getAllCategories = async function (req, res) {
+  getAllEntries(res, req.app.client).catch((error) => {
+    res
+      .status(400)
+      .end("Error in the get all categories request: " + error.message);
+  });
+};
+
+exports.updateCategory = async function (req, res) {
+  const query = req.body;
+
+  console.log(query);
+  updateEntry(res, req.app.client, name, update).catch((error) => {
+    res
+      .status(400)
+      .end("Error in the update category request: " + error.message);
+  });
+};
+
+async function createEntry(res, client, entry) {
+  const result = await client
+    .db("CatsProds")
+    .collection("Categories")
+    .insertOne(entry);
+  res.sendStatus(200);
+}
+
+async function getEntry(res, client, name) {
+  const result = await client
+    .db("CatsProds")
+    .collection("Categories")
+    .findOne({ name: name });
+
+  res.send(result);
+}
+
+async function getAllEntries(res, client) {
+  const result = await client
+    .db("CatsProds")
+    .collection("Categories")
+    .find({})
+    .toArray(function (err, docs) {
+      res.send(docs);
+    });
+}
+
+async function updateEntry(res, client, name, update) {
+  const result = await client
+    .db("CatsProds")
+    .collection("Categories")
+    .updateOne({ name: name }, { $set: update }, function (err, result) {
+      assert.equal(err, null);
+      res.sendStatus(200);
+    });
+}
---
+++ services/productsService.js
@@ -0,0 +1,107 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const validate = require("jsonschema").validate;
+
+// Schema to compare new POST entries to
+const prodSchema = {
+  name: "string",
+  price: "double",
+  categories: {
+    type: "array",
+    items: { type: "string" },
+  },
+  required: ["name", "price", "categories"],
+};
+
+exports.postProduct = async function (req, res) {
+  const entry = req.body;
+
+  try {
+    validate(entry, prodSchema, { throwError: true });
+  } catch (error) {
+    res.status(401).end("Invalid body format: " + error.message);
+    return;
+  }
+
+  console.log(entry);
+  createEntry(res, req.app.client, entry).catch((error) => {
+    res
+      .status(400)
+      .end("Error in the create product request: " + error.message);
+  });
+};
+
+exports.getProduct = async function (req, res) {
+  const query = req.query;
+  const name = query.name;
+
+  getEntry(res, req.app.client, name).catch((error) => {
+    res.status(400).end("Error in get product request: " + error.message);
+  });
+};
+
+exports.getCategoryProducts = async function (req, res) {
+  const query = req.query;
+  const name = query.name;
+
+  getProductsFromCategory(res, req.app.client, category).catch((error) => {
+    res
+      .status(400)
+      .end("Error in the get products by category request: " + error.message);
+  });
+};
+
+exports.updateProduct = async function (req, res) {
+  const query = req.body;
+
+  console.log(query);
+  updateEntry(res, req.app.client, name, update).catch((error) => {
+    res
+      .status(400)
+      .end("Error in the update product request: " + error.message);
+  });
+};
+
+async function createEntry(res, client, entry) {
+  const result = await client
+    .db("CatsProds")
+    .collection("Products")
+    .insertOne(entry);
+
+  res.sendStatus(200);
+}
+
+async function getEntry(res, client, name) {
+  const result = await client
+    .db("CatsProds")
+    .collection("Products")
+    .findOne({ name: name });
+
+  res.send(result);
+}
+
+async function getProductsFromCategory(res, client, category) {
+  const result = await client
+    .db("CatsProds")
+    .collection("Products")
+    .find({ categories: category })
+    .toArray(function (err, docs) {
+      console.log("Found the following records");
+      console.log(docs);
+      res.send(docs);
+    });
+}
+
+async function updateEntry(res, client, name, update) {
+  const result = await client
+    .db("CatsProds")
+    .collection("Products")
+    .updateOne({ name: name }, { $set: update }, function (err, result) {
+      assert.equal(err, null);
+      console.log(`Updated the product with the field name equal to ${name}`);
+    });
+
+  res.sendStatus(200);
+}
